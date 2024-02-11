5a88cba6aac00125decd52d7e1398f947bfca13e
Artyom Rogov <artyomrogov2012@gmail.com>
2024-02-11 13:43:34 +0200

    Для квадратных клипов теперь идет плавное увеличение с фоном города

--- clips/position.py
+++ clips/position.py
@@ -1,3 +1,5 @@
+import random
+
 import moviepy.editor as mp
 import moviepy.video.fx.all as vfx
 import moviepy.audio.fx.all as afx
@@ -12,6 +14,7 @@ number_font = 'Microsoft-PhagsPa-Bold'
 # Bakbak-One-Regular
 
 chart_number_font = 'GraphikLCG-BlackItalic'
+city_video_path = 'package/city_blur1.mp4'
 
 
 def create_position_clip(
@@ -34,9 +37,30 @@ def create_position_clip(
 		return mp.VideoFileClip(filename='video_parts/' + result_name + '.mp4')
 
 	print('Start creating position ' + str(position) + ', name: ' + result_name)
-	clip1 = mp.VideoFileClip(clip_path, target_resolution=(1080, 1920))
+	clip1 = mp.VideoFileClip(clip_path)
 	music_clip = clip1.subclip(clip_start_time, clip_end_time)
 	duration = music_clip.duration
+	clip_width, clip_height = music_clip.size
+
+	background_video = None
+	if clip_width == clip_height:
+		print('render 1:1 video with bg')
+		bg_video = mp.VideoFileClip(city_video_path, target_resolution=(1080, 1920))
+		bg_start_time = random.random() * bg_video.duration * 0.9
+		bg_end_time = bg_start_time + duration
+		background_video = bg_video.subclip(bg_start_time, bg_end_time)
+		start_size = 0.85
+		end_size = 0.9
+		music_clip = music_clip \
+			.set_position(('center', 'center')) \
+			.resize(start_size) \
+			.resize(lambda t: 1 + (end_size - start_size) / (start_size * duration) * t)
+	elif clip_height != 1080 and clip_width != 1920:
+		print('render standart Full HD video')
+		music_clip.resize((1080, 1920))
+	else:
+		print('source clip is FHD already')
+
 	w, h = 1920, 1080
 
 	clip_name = mp.TextClip(
@@ -115,6 +139,8 @@ def create_position_clip(
 		clip_position.crossfadein(0.4).crossfadeout(0.4),
 		tcc_logo,
 	]
+	if background_video:
+		clip_list.insert(0, background_video)
 	if arrow_path and arrow:
 		clip_list.append(arrow.set_start(duration - 3).crossfadein(0.4).crossfadeout(0.4))
 	if show_stats and clip_stats:
@@ -162,9 +188,26 @@ def create_last_out_clip(
 		moving, show_stats, result_name,
 	)
 
-	music_clip = mp.VideoFileClip(clip_path, target_resolution=(1080, 1920)) \
+	music_clip = mp.VideoFileClip(clip_path) \
 		.subclip(clip_end_time - 1.2, clip_end_time + 2) \
 		.fl_image(blur)
+	duration = music_clip.duration
+	clip_width, clip_height = music_clip.size
+
+	background_video = None
+	if clip_width == clip_height:
+		bg_video = mp.VideoFileClip(city_video_path, target_resolution=(1080, 1920))
+		bg_start_time = random.random() * bg_video.duration * 0.9
+		bg_end_time = bg_start_time + duration
+		background_video = bg_video.subclip(bg_start_time, bg_end_time)
+		start_size = 0.9
+		end_size = 0.87
+		music_clip = music_clip \
+			.set_position(('center', 'center')) \
+			.resize(start_size) \
+			.resize(lambda t: 1 + (end_size - start_size) / (start_size * duration) * t)
+	elif clip_height != 1080 and clip_width != 1920:
+		music_clip.resize((1080, 1920))
 
 	clip_chart_number = mp.TextClip(
 		txt='#' + str(chart_number), font=chart_number_font,
@@ -208,18 +251,21 @@ def create_last_out_clip(
 		.crossfadein(0.4) \
 		.crossfadeout(0.4)
 
-	blurred_clip = mp.CompositeVideoClip([
+	clip_list = [
 		music_clip,
 		clip_chart_number,
 		clip_chart_date,
 		clip_logo,
 		clip_site,
 		clip_ep_logo,
-	])
+	]
+	if background_video:
+		clip_list.insert(0, background_video)
+	blurred_clip = mp.CompositeVideoClip(clip_list)
 
 	final = mp.CompositeVideoClip([
 		clip.fx(afx.audio_fadeout, 0.2),
-		blurred_clip.crossfadein(0.2).set_start(clip.duration-0.2).fx(afx.audio_fadein, 0.2)
+		blurred_clip.crossfadein(0.2).set_start(clip.duration - 0.2).fx(afx.audio_fadein, 0.2)
 	])
 
 	if need_render:
