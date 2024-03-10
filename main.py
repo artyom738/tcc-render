2e187834abbdecea5ed495f955512c829706b0a2
Artyom Rogov <artyomrogov2012@gmail.com>
2024-03-10 22:52:36 +0200

    Новый чартик ехт

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from datetime import datetime
 
-from charts.eht_40 import Eht40
-from charts.top_club_chart import TopClubChart
+from charts.factory import ChartFactory
 from model.entity.chart import Chart
 
 
 def __main__(chart: Chart):
-	TopClubChart(chart).render()
-	# Eht40(chart).render()
+	renderable_chart = ChartFactory().create_chart(chart)
+	if renderable_chart:
+		renderable_chart.render()
+
 
 if __name__ == '__main__':
 	# Chart date format YYYY-MM-DD
 	chart = Chart(data={
-		'chart_number': 456,
-		'chart_date': datetime(2024, 3, 9),
-		'chart_type': 'tcc',
-		# 'chart_type': 'eht',
+		'chart_number': 1,
+		'chart_date': datetime(2024, 3, 8),
+		# 'chart_type': 'tcc',
+		'chart_type': 'eht',
 	})
 	__main__(chart)
