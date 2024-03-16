938878dc8b1050a6e488b411e3cf27da2d3d5821
Artyom Rogov <artyomrogov2012@gmail.com>
2024-03-16 15:50:21 +0200

    Переделал логику подбора кусочка клипа - постепенное уменьшение порога амплитуды для подбора самых подходящих кусочков

--- chorus_finder.py
+++ chorus_finder.py
@@ -12,22 +12,41 @@ def analyze_track(audio_path: str, draw_chart=False):
 	# audio_path = 'test_results/!York - On The Beach (Kryder Remix).mp3'
 	result = dict()
 	y, sr = librosa.load(audio_path)
+	full_duration = librosa.get_duration(y=y)
 
 	# Calculate the amplitude envelope
 	amplitude_envelope = librosa.feature.rms(y=y)[0]
 
 	average_amplitude = round(np.mean(amplitude_envelope), 2)
+	max_amplitude = round(np.max(amplitude_envelope), 2)
 	result['average_amplitude'] = average_amplitude
-	# Set a threshold for detecting increasing energy
-	threshold = round(average_amplitude * 1.7, 2)  # Adjust this value based on your analysis
+	result['max_amplitude'] = max_amplitude
 
-	result['threshold'] = threshold
+	energy_multiplicator = max_amplitude / average_amplitude
+	while True:
+		# Set a threshold for detecting increasing energy
+		threshold = round(average_amplitude * energy_multiplicator, 2)  # Adjust this value based on your analysis
+		result['threshold'] = threshold
+		# Find indices where the amplitude exceeds the threshold
+		increasing_energy_indices = np.where(amplitude_envelope > threshold)[0]
+		# time when amplitude > threshold
+		time_chorus = librosa.times_like(amplitude_envelope)[increasing_energy_indices]
 
-	# Find indices where the amplitude exceeds the threshold
-	increasing_energy_indices = np.where(amplitude_envelope > threshold)[0]
+		start_times = []
+		for index in range(len(time_chorus) - 1):
+			if time_chorus[index] + 1 < time_chorus[index + 1]:
+				if (len(start_times) == 0 or time_chorus[index + 1] > start_times[-1] + 10) \
+						and time_chorus[index + 1] + 20 < full_duration \
+						and time_chorus[index + 1] > 30:
+					start_times.append(round(time_chorus[index + 1], 2))
+		if len(start_times) > 2:
+			break
+		if energy_multiplicator > 2:
+			energy_multiplicator -= 0.2
+		else:
+			energy_multiplicator -= 0.1
 
-	# time when amplitude > threshold
-	time_chorus = librosa.times_like(amplitude_envelope)[increasing_energy_indices]
+	result['start_times'] = start_times
 
 	bpm, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 	result['bpm'] = round(bpm, 2)
@@ -37,17 +56,6 @@ def analyze_track(audio_path: str, draw_chart=False):
 		tacts -= 2
 
 	piece_duration = 60 / bpm * tacts
-	full_duration = librosa.get_duration(y=y)
-
-	start_times = []
-	for index in range(len(time_chorus) - 1):
-		if time_chorus[index] + 1 < time_chorus[index + 1]:
-			if (len(start_times) == 0 or time_chorus[index + 1] > start_times[-1] + 10) \
-					and time_chorus[index + 1] + 20 < full_duration \
-					and time_chorus[index + 1] > 30:
-				start_times.append(round(time_chorus[index + 1], 2))
-
-	result['start_times'] = start_times
 
 	end_times = []
 	for start_time in start_times:
