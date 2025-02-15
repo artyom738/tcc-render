abf999b1975c5fe866ca82abb1736ae7faae75cf
Artyom Rogov <artyomrogov2012@gmail.com>
2025-02-15 22:53:38 +0200

    Рендера листа по очкам

--- charts/list.py
+++ charts/list.py
@@ -5,6 +5,7 @@ from datetime import datetime, date
 
 from charts.base_chart import BaseChart
 from clips.position import chart_number_font, text_font
+from model.repository.chart_repository import chart_repository
 
 
 class List(BaseChart):
@@ -21,32 +22,41 @@ class List(BaseChart):
 		return True
 
 	def get_additional_stat_info(self, song: 'Song'):
-		charts = song.get_charts(self.get_chart_type())
+		# charts = song.get_charts('eht')
+		ny_chart = chart_repository.get_chart_by_id(205).fill()
+		position_info = 'В итогах не был'
+		for ny_chart_position in ny_chart.positions:
+			if ny_chart_position.song_id == song.id:
+				position_info = f'В итогах: #{ny_chart_position.position}'
 
-		if len(charts) == 0:
-			return 'Не был в чарте'
+		points_info = f'Очков в 2024: {song.get_points("tcc", datetime.strptime("2024-01-01", "%Y-%m-%d"), datetime.strptime("2025-01-01", "%Y-%m-%d"))}'
 
-		first = charts[0]
-		last = charts[len(charts) - 1]
+		return position_info + '\n' + points_info
 
-		if last['CHART_DATE'].strftime('%Y-%m-%d') == '2024-12-06':
-			date_range = first['CHART_DATE'].strftime('%d.%m.%Y') + ' - наст. вр.'
-		else:
-			date_range = first['CHART_DATE'].strftime('%d.%m.%Y') + ' - ' + last['CHART_DATE'].strftime('%d.%m.%Y')
-
-		if first['CHART_DATE'] < datetime.strptime('2024-01-01', '%Y-%m-%d').date():
-			weeks_in_2024 = 0
-			for chart in charts:
-				if chart['CHART_DATE'] > datetime.strptime('2024-01-01', '%Y-%m-%d').date():
-					weeks_in_2024 += 1
-			return 'Недель в 2024 - ' + str(weeks_in_2024) + "\n" + date_range
-
-		return date_range
+		# if len(charts) == 0:
+		# 	return 'Не был в чарте'
+		#
+		# first = charts[0]
+		# last = charts[len(charts) - 1]
+		#
+		# if last['CHART_DATE'].strftime('%Y-%m-%d') == '2024-12-06':
+		# 	date_range = first['CHART_DATE'].strftime('%d.%m.%Y') + ' - наст. вр.'
+		# else:
+		# 	date_range = first['CHART_DATE'].strftime('%d.%m.%Y') + ' - ' + last['CHART_DATE'].strftime('%d.%m.%Y')
+		#
+		# if first['CHART_DATE'] < datetime.strptime('2024-01-01', '%Y-%m-%d').date():
+		# 	weeks_in_2024 = 0
+		# 	for chart in charts:
+		# 		if chart['CHART_DATE'] > datetime.strptime('2024-01-01', '%Y-%m-%d').date():
+		# 			weeks_in_2024 += 1
+		# 	return 'Недель в 2024 - ' + str(weeks_in_2024) + "\n" + date_range
+		#
+		# return date_range
 
 	def get_intro(self):
 		#  May be changed due to variable charts
 		return mp.VideoFileClip(
-			'package/eht/intro.mp4',
+			'package/tcc/intro.mp4',
 			target_resolution=(1080, 1920)
 		)
 
