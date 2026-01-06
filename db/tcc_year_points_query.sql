661cd1ecce03839a194e7197858490873ac91fd3
Artyom Rogov <mrartyomka@gmail.com>
2026-01-06 22:17:45 +0200

    Отдельные запросы для ехт и ткч по очкам

---
+++ db/tcc_year_points_query.sql
@@ -0,0 +1,17 @@
+SELECT
+    s.ID,
+    s.AUTHORS AS 'Автор',
+    s.NAME AS 'Название',
+    SUM(26 - cp.POSITION) AS 'Очки'
+FROM
+    songs s
+    INNER JOIN chart_positions cp ON s.ID = cp.SONG_ID
+    INNER JOIN charts c ON cp.CHART_ID = c.ID
+WHERE
+    c.CHART_TYPE = 'tcc'
+    AND c.CHART_DATE >= '2025-01-01'
+    AND c.CHART_DATE < '2026-01-01'
+GROUP BY
+    s.ID, s.AUTHORS, s.NAME
+ORDER BY
+    Очки DESC;
